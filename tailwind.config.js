8010e7574a52b0ed15b6fb9e66ec93693f56f40d
MJARuijs <mja.ruijs@gmail.com>
2024-08-05 14:37:39 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      roboto_thin: ['Roboto-Thin'],
+      roboto_light: ['Roboto-Light'],
+      roboto_regular: ['Roboto-Regular'],
+      roboto_medium: ['Roboto-Medium'],
+      roboto_bold: ['Roboto-Bold'],
+      roboto_black: ['Roboto-Black'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
